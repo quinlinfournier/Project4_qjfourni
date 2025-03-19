8e2d2baba951b6c2cf02ed5f29fdf3c3a57c5e98
quinlinfournier <123664556+quinlinfournier@users.noreply.github.com>
2025-03-19 10:15:45 -0400

    Initial commit

---
+++ sorting-algorithms/bubbleSort.h
@@ -0,0 +1,33 @@
+#ifndef SORTING_BUBBLESORT_H
+#define SORTING_BUBBLESORT_H
+
+#include "printVec.h"
+
+template<typename Comparable>
+vector<Comparable> bubbleSort(vector<Comparable> vec, unsigned long& reads, unsigned long& allocations) {
+    reads = allocations = 0;
+    int numPasses = 0, i;
+    Comparable temp;
+    bool haveSwapped = true;
+    while (haveSwapped) {
+        haveSwapped = false;
+        for (i = 0; i+1 < vec.size()-numPasses; ++i) {
+            // Compare items at indices i and i+1 and swap if necessary
+            if (vec[i] > vec[i+1]) {
+                temp = vec[i];
+                vec[i] = vec[i+1];
+                vec[i+1] = temp;
+                // Update haveSwapped
+                haveSwapped = true;
+            }
+        }
+        // Update numPasses
+        ++numPasses;
+
+        // Uncomment this line if you want to see each iteration
+        //printVec(vec);
+    }
+    return vec;
+}
+
+#endif
